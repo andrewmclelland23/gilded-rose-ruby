81dcaa16c71333f6699ba05e2217aa2e5ec323b5
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-05-10 17:11:33 +0100

    REFACTOR: Changed to one methods for updating standard items and another for non standard

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,63 +2,51 @@ require_relative 'item'
 
 class GildedRose
 
+  NON_STANDARD_ITEMS = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]
+  STATIC_ITEMS = ["Sulfuras, Hand of Ragnaros"]
+
   def initialize(items)
     @items = items
   end
 
   def update_items
     @items.each do |item|
-      next if item.name == "Sulfuras, Hand of Ragnaros"
+      next if STATIC_ITEMS.include? item.name
 
-      update_quality_standard(item)
       update_sell_in(item)
-      update_quality_post_sell_in(item)
+      if NON_STANDARD_ITEMS.include? item.name
+        update_quality_non_standard(item)
+      else
+        update_quality_standard(item)
+      end
     end
   end
 
   def update_quality_standard(item)
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality > 0
-        item.quality = item.quality - 1
+    quality_change = item.sell_in < 1 ? 2 : 1
+    item.quality -= quality_change
+    item.quality = 0 if item.quality < 0
+  end
+
+  def update_quality_non_standard(item)
+    item.quality = item.quality + 1
+    if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      if item.sell_in <= 10
+        item.quality = item.quality + 1
+      end
+      if item.sell_in <= 5
+        item.quality = item.quality + 1
+      end
+      if item.sell_in <= 0
+        item.quality = 0
       end
     else
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
-      end
+      item.quality = item.quality + 1 if item.sell_in < 1
     end
+    item.quality = 50 if item.quality > 50
   end
 
-  def update_quality_post_sell_in(item)
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
+    def update_sell_in(item)
+      item.sell_in -= 1
     end
-  end
-
-  def update_sell_in(item)
-    item.sell_in = item.sell_in - 1
-  end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,68 +2,76 @@ require 'gilded_rose'
 
 describe GildedRose do
 
+  subject(:gilded_rose) { described_class }
+
   describe "#update_sell_in" do
 
     it 'should reduce sell_in by 1' do
       item = Item.new("foo", 5, 5)
-      GildedRose.new([item]).update_sell_in(item)
+      gilded_rose.new([item]).update_sell_in(item)
       expect(item.sell_in).to eq 4
     end
   end
 
-  describe "#update_items" do
+  describe "#update_quality_standard" do
 
-    context "normal item" do
-      context "sell_in > 0, quality > 0" do
-        it 'should reduce quality by 1' do
-          items = [Item.new("foo", 5, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 4
-        end
-      end
-      context "sell_in = 0, quality > 0" do
-        it 'should reduce quality by 2' do
-          items = [Item.new("foo", 0, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 3
-        end
-      end
-      context "quality = 0" do
-        it 'should not reduce quality' do
-          items = [Item.new("foo", 5, 0)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 0
-        end
-      end
-      context "sell_in = 0, quality = 1" do
-        it 'should reduce quality by 1' do
-          items = [Item.new("foo", 0, 1)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 0
-        end
+    context "sell_in > 1, quality > 0" do
+      it 'should reduce quality by 1' do
+        item = Item.new("foo", 2, 5)
+        gilded_rose.new([item]).update_quality_standard(item)
+        expect(item.quality).to eq 4
       end
     end
+    context "sell_in < 1, quality > 0" do
+      it 'should reduce quality by 2' do
+        item = Item.new("foo", 0, 5)
+        gilded_rose.new([item]).update_quality_standard(item)
+        expect(item.quality).to eq 3
+        item = Item.new("foo", -1, 5)
+        gilded_rose.new([item]).update_quality_standard(item)
+        expect(item.quality).to eq 3
+      end
+    end
+    context "quality = 0" do
+      it 'should not reduce quality' do
+        item = Item.new("foo", 0, 0)
+        gilded_rose.new([item]).update_quality_standard(item)
+        expect(item.quality).to eq 0
+      end
+    end
+  end
 
+  describe "#update_quality_non_standard" do
     context "Aged Brie item" do
       context "sell_in > 0, quality < 50" do
         it 'should increase quality by 1' do
-          items = [Item.new("Aged Brie", 5, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 6
+          item = Item.new("Aged Brie", 5, 5)
+          gilded_rose.new([item]).update_quality_non_standard(item)
+          expect(item.quality).to eq 6
         end
       end
       context "sell_in < 1, quality < 49" do
         it 'should increase quality by 2' do
-          items = [Item.new("Aged Brie", 0, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 7
+          item = Item.new("Aged Brie", 0, 48)
+          gilded_rose.new([item]).update_quality_non_standard(item)
+          expect(item.quality).to eq 50
+          item = Item.new("Aged Brie", -1, 48)
+          gilded_rose.new([item]).update_quality_non_standard(item)
+          expect(item.quality).to eq 50
         end
       end
       context "quality = 50" do
         it 'should not change quality' do
-          items = [Item.new("Aged Brie", 5, 50)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 50
+          item = Item.new("Aged Brie", 5, 50)
+          gilded_rose.new([item]).update_quality_non_standard(item)
+          expect(item.quality).to eq 50
+        end
+      end
+      context "quality = 49, sell_in < 1" do
+        it 'should add 1 to quality (not 2) due to max possible being 50' do
+          item = Item.new("Aged Brie", 0, 49)
+          gilded_rose.new([item]).update_quality_non_standard(item)
+          expect(item.quality).to eq 50
         end
       end
     end
@@ -71,61 +79,76 @@ describe GildedRose do
     context "Backstage passes" do
       context "sell_in > 10, quality < 50" do
         it 'should increase quality by 1' do
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 6
+          item = Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 5)
+          gilded_rose.new(item).update_quality_non_standard(item)
+          expect(item.quality).to eq 6
         end
       end
-      context "5 < sell_in <= 10, quality < 49" do
+      context "5 < sell_in < 10, quality < 49" do
         it 'should increase quality by 2' do
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 7
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 7
+          item = Item.new("Backstage passes to a TAFKAL80ETC concert", 9, 5)
+          gilded_rose.new(item).update_quality_non_standard(item)
+          expect(item.quality).to eq 7
+          item = Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 5)
+          gilded_rose.new(item).update_quality_non_standard(item)
+          expect(item.quality).to eq 7
         end
       end
       context "0 < sell_in <= 5, quality < 49" do
         it 'should increase quality by 3' do
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 8
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 8
+          item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 5)
+          gilded_rose.new(item).update_quality_non_standard(item)
+          expect(item.quality).to eq 8
+          item = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 5)
+          gilded_rose.new(item).update_quality_non_standard(item)
+          expect(item.quality).to eq 8
         end
       end
       context "sell_in = 0" do
         it 'should reduce quality to 0' do
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 0
+          item = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 5)
+          gilded_rose.new(item).update_quality_non_standard(item)
+          expect(item.quality).to eq 0
         end
       end
     end
+  end
 
-    context "Sulfuras item" do
-      context "sell_in > 0, quality < 50" do
-        it 'should not change quality' do
-          items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 5
-        end
+  describe '#update_items' do
+
+    before(:each) do
+      stub_const("GildedRose::STATIC_ITEMS", ["static item"])
+      stub_const("GildedRose::NON_STANDARD_ITEMS", ["non standard item"])
+    end
+
+    context 'only item in items is included in STATIC_ITEMS' do
+      it 'should not call any of the update methods on static item' do
+        items = [Item.new("static item", 5, 5)]
+        gr = gilded_rose.new(items)
+        expect(gr).to_not receive :update_quality_standard
+        expect(gr).to_not receive :update_quality_non_standard
+        expect(gr).to_not receive :update_sell_in
+        gr.update_items
       end
-      context "sell_in < 1, quality < 49" do
-        it 'should not change quality' do
-          items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 5)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 5
-        end
+    end
+    context 'only item in items is included in NON_STANDARD_ITEMS' do
+      it 'should call update_sell_in and update_quality_non_standard' do
+        items = [Item.new("non standard item", 5, 5)]
+        gr = gilded_rose.new(items)
+        expect(gr).to_not receive :update_quality_standard
+        expect(gr).to receive :update_quality_non_standard
+        expect(gr).to receive :update_sell_in
+        gr.update_items
       end
-      context "quality = 50" do
-        it 'should not change quality' do
-          items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 50)]
-          GildedRose.new(items).update_items
-          expect(items[0].quality).to eq 50
-        end
+    end
+    context 'only item in items is standard item' do
+      it 'should call update_sell_in and update_quality_standard' do
+        items = [Item.new("standard item", 5, 5)]
+        gr = gilded_rose.new(items)
+        expect(gr).to receive :update_quality_standard
+        expect(gr).to_not receive :update_quality_non_standard
+        expect(gr).to receive :update_sell_in
+        gr.update_items
       end
     end
   end
